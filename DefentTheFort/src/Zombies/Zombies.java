8ebe5d6ccd989b8784e3019d5247fdb43d647304
KennethRojas14 <rojasjimenezkenneth@estudiantec.cr>
2022-10-27 10:44:37 -0600

    Se añadió clase zombies

---
+++ DefentTheFort/src/Zombies/Zombies.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Zombies;
+
+import java.awt.Image;
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author 167069
+ */
+public abstract class Zombies {
+    private int vida;
+    private int ataque;
+    private int campos;
+    private String nombre;
+    private JButton btnPosicion;
+    private Image imagen;
+    
+    
+    public Zombies(int vida, int ataque, int campos, String nombre, Image imagen, JButton posicion) {
+        this.vida = vida;
+        this.ataque = ataque;
+        this.campos = campos;
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.btnPosicion = posicion;
+        //Falta Arrays
+    }
+    public void morir(){
+        setVida(0);
+    }
+    public abstract void atacar();
+    
+    public void aparecer(){ //Pone la imagen
+        this.btnPosicion.setIcon((Icon) imagen);
+    }
+    
+    public void desaparecer(){ //Quita la imagen
+        this.btnPosicion.setDisabledIcon((Icon) imagen);
+    }
+    
+    
+    public int getVida() {
+        return vida;
+    }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    public int getCampos() {
+        return campos;
+    }
+    public void setCampos(int campos) {
+        this.campos = campos;
+    }
+    public Image getImagen() {
+        return imagen;
+    }
+    public void setImagen(Image imagen) {
+        this.imagen = imagen;
+    }
+    public int getAtaque() {
+        return ataque;
+    }
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public JButton getBtnPosicion() {
+        return btnPosicion;
+    }
+    public void setBtnPosicion(JButton txfPosicion) {
+        this.btnPosicion = txfPosicion;
+    }   
+}
